3fbb8adf34b046a927b3d75b2dfbbf6ac8d0eec0
Mikhail Kalinin <noblesse.knight@gmail.com>
2020-07-18 02:32:11 +0600

    Do not verify BLS for deposits in simulation

--- phase1/src/main/kotlin/tech/pegasys/teku/phase1/simulation/util/GenesisUtil.kt
+++ phase1/src/main/kotlin/tech/pegasys/teku/phase1/simulation/util/GenesisUtil.kt
@@ -1,6 +1,7 @@
 package tech.pegasys.teku.phase1.simulation.util
 
 import tech.pegasys.teku.bls.BLSKeyPair
+import tech.pegasys.teku.datastructures.util.BeaconStateUtil
 import tech.pegasys.teku.datastructures.util.MockStartBeaconStateGenerator
 import tech.pegasys.teku.datastructures.util.MockStartDepositGenerator
 import tech.pegasys.teku.phase1.integration.datastructures.BeaconState
@@ -11,6 +12,7 @@ import tech.pegasys.teku.phase1.onotole.phase1.get_forkchoice_store
 import tech.pegasys.teku.phase1.onotole.phase1.upgrade_to_phase1
 
 fun getGenesisState(blsKeyPairs: List<BLSKeyPair>): BeaconState {
+  BeaconStateUtil.BLS_VERIFY_DEPOSIT = false
   val deposits = MockStartDepositGenerator().createDeposits(blsKeyPairs)
   val state = upgrade_to_phase1(
     MockStartBeaconStateGenerator().createInitialBeaconState(
