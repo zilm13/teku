850b1b5bacaf57ef693953cc18e65074c961f3c3
Mikhail Kalinin <noblesse.knight@gmail.com>
2020-07-21 19:03:41 +0600

    Change Eth1 block encoding to RLP

--- phase1/src/main/kotlin/tech/pegasys/teku/phase1/eth1client/Eth1BlockUtil.kt
+++ phase1/src/main/kotlin/tech/pegasys/teku/phase1/eth1client/Eth1BlockUtil.kt
@@ -1,6 +1,7 @@
 package tech.pegasys.teku.phase1.eth1client
 
 import org.apache.tuweni.crypto.Hash
+import org.apache.tuweni.rlp.RLP
 import tech.pegasys.teku.phase1.onotole.ssz.Bytes
 import tech.pegasys.teku.phase1.onotole.ssz.Bytes32
 import tech.pegasys.teku.phase1.onotole.ssz.uint64
@@ -14,19 +15,24 @@ fun encodeBlockDataWithRLP(
   receiptsRoot: Bytes32,
   body: Bytes
 ): Bytes {
-  return Bytes.concatenate(
-    Bytes.ofUnsignedLong(number.toLong()),
-    parentHash,
-    stateRoot,
-    receiptsRoot,
-    body
-  )
+  return RLP.encodeList {
+    it.writeLong(number.toLong())
+    it.writeValue(parentHash)
+    it.writeValue(stateRoot)
+    it.writeValue(receiptsRoot)
+    it.writeValue(body)
+  }
 }
 
 fun parseBlockNumberFromRLP(rlp: Bytes): uint64 {
-  return rlp.slice(0, 8).toLong().toULong()
+  return RLP.decodeList(rlp) {
+    it.readLong().toULong()
+  }
 }
 
 fun parseParentHashFromRLP(rlp: Bytes): Bytes32 {
-  return Bytes32.wrap(rlp.slice(8, Bytes32.SIZE))
+  return Bytes32.wrap(RLP.decodeList(rlp) {
+    it.readLong()
+    return@decodeList it.readValue()
+  })
 }
