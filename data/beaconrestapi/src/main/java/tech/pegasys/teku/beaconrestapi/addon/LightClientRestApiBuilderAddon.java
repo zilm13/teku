64780023c4e81bb529fab0bbbbd4be2a8b897039
Abhivansh <31AbhivanshJ@gmail.com>
2026-09-16 10:11:49 +0530

    Implement `/eth/v1/beacon/light_client/finality_update` (#11243)
    * chore: add initial scaffold for GetLightClientFinalityUpdate
    
    * chore: implement handle request
    
    * chore: add optional checks in handleRequest
    
    * test: add GetLightClientFinalityUpdate tests
    
    * chore: add GetLightClientFinalityUpdate json file
    
    * chore: add endpoint in rest api builder
    
    * fix: fix the route
    
    * format the json file
    
    * fix: fix per milestone schema bug
    
    * chore: add other json files
    
    * chore: make the schema fork test more robust
    
    * chore: add integration tests
    
    * fix: minor fixes
    
    * address feedback
    
    ---------
    
    Signed-off-by: Abhivansh  <31AbhivanshJ@gmail.com>
    Co-authored-by: Paul Harris <paul.harris@consensys.net>

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/addon/LightClientRestApiBuilderAddon.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/addon/LightClientRestApiBuilderAddon.java
@@ -17,6 +17,7 @@ import tech.pegasys.teku.api.DataProvider;
 import tech.pegasys.teku.beaconrestapi.BeaconRestApiConfig;
 import tech.pegasys.teku.beaconrestapi.RestApiBuilderAddon;
 import tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.lightclient.GetLightClientBootstrap;
+import tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.lightclient.GetLightClientFinalityUpdate;
 import tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.lightclient.GetLightClientUpdatesByRange;
 import tech.pegasys.teku.infrastructure.restapi.RestApiBuilder;
 import tech.pegasys.teku.spec.schemas.SchemaDefinitionCache;
@@ -45,6 +46,7 @@ public class LightClientRestApiBuilderAddon implements RestApiBuilderAddon {
   public RestApiBuilder apply(final RestApiBuilder builder) {
     return builder
         .endpoint(new GetLightClientBootstrap(dataProvider, schemaCache))
-        .endpoint(new GetLightClientUpdatesByRange(dataProvider, schemaCache));
+        .endpoint(new GetLightClientUpdatesByRange(dataProvider, schemaCache))
+        .endpoint(new GetLightClientFinalityUpdate(dataProvider, schemaCache));
   }
 }
